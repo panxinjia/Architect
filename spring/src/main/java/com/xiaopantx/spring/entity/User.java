20f89c20f65da67f24a6cee5dbabbcfe97a86686
m15132582590@163.com <w19948241994824w>
2021-07-15 20:10:00 +0800

    init

--- spring/src/main/java/com/xiaopantx/spring/entity/User.java
+++ spring/src/main/java/com/xiaopantx/spring/entity/User.java
@@ -8,6 +8,15 @@ public class User {
     private Integer id;
     private String name;
 
+    public User() {
+
+    }
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     @Override
     public String toString() {
         return "User{" +
--- spring/src/main/java/com/xiaopantx/spring/test/MainTest.java
+++ spring/src/main/java/com/xiaopantx/spring/test/MainTest.java
@@ -13,9 +13,12 @@ public class MainTest {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("beans.xml");
         // 类型获取Bean
-        User user = applicationContext.getBean(User.class);
+        User user = (User) applicationContext.getBean("user");
+        User user2 = (User) applicationContext.getBean("user2");
         String content = ReflectionToStringBuilder.toString(user);
+        String content2 = ReflectionToStringBuilder.toString(user2);
         System.out.println(content);
+        System.out.println(content2);
         System.out.println("------------------------------------------");
         // bean 别名
         String[] aliases = applicationContext.getAliases("user");
